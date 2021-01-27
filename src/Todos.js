ab10e43d85b558f4d8559f53a1e84e6f3a90d852
Asisipho <sipho.msitho@gmail.com>
2021-01-27 19:52:19 +0200

    edited two files

--- src/Todos.js
+++ src/Todos.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Todos = ({todos, deleteTodo}) => {
 
-    const todoList = todos.length ? (guit
+    const todoList = todos.length ? (
         todos.map(todo => {
             return(
                 <div className="collection-item" key={todo.id}>
